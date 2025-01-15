717ad39b20deaed75822a6fd81eac12272097f37
Diana Shilova <d.shilova@redmadrobot.com>
2025-01-15 14:40:18 +0300

    frontend initialization

---
+++ client/src/pages/Home/Home.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import './login.css';
+import { useNavigate } from 'react-router';
+
+const Home: React.FC = () => {
+  
+  return (
+    <div className='auth-container'>
+      <div className='auth-form'>
+        <h1>Where is my money ?</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
